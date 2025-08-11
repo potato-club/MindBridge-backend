baad31a44a11732a34c648cc77ca46139db5d78f
budoli <als01540@gmail.com>
2025-08-11 15:54:55 +0900

    first commit

---
+++ src/main/java/com/mindbridge/MindbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.mindbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindbridgeApplication.class, args);
+	}
+
+}
