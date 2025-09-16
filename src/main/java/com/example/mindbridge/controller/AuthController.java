6ba297be8f59f550664a931111eb2dd2a7f714f2
iamingyun <mingyun0516@naver.com>
2025-09-16 18:27:43 +0900

    remove: 회원가입 코드 삭제

--- src/main/java/com/example/mindbridge/controller/AuthController.java
+++ src/main/java/com/example/mindbridge/controller/AuthController.java
@@ -2,11 +2,7 @@ package com.example.mindbridge.controller;
 
 import com.example.mindbridge.dto.ApiResponseDTO;
 import com.example.mindbridge.dto.LoginRequestDTO;
-import com.example.mindbridge.dto.SignupRequestDTO;
-import com.example.mindbridge.entity.UserEntity;
 import com.example.mindbridge.service.AuthService;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +16,6 @@ public class AuthController {
 
     public AuthController(AuthService authService) {
         this.authService = authService;
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<ApiResponseDTO<UserEntity>> signup(@Valid @RequestBody SignupRequestDTO req) {
-       UserEntity user = authService.signup(req);
-        return ResponseEntity.ok(
-                new ApiResponseDTO<>(true, "회원가입이 성공적으로 완료되었습니다.", user)
-        );
     }
 
     @PostMapping("/login")
--- src/main/java/com/example/mindbridge/service/AuthService.java
+++ src/main/java/com/example/mindbridge/service/AuthService.java
@@ -2,12 +2,9 @@ package com.example.mindbridge.service;
 
 import com.example.mindbridge.dto.ApiResponseDTO;
 import com.example.mindbridge.dto.LoginRequestDTO;
-import com.example.mindbridge.dto.SignupRequestDTO;
 import com.example.mindbridge.entity.UserEntity;
 import com.example.mindbridge.repository.UserRepository;
 import com.example.mindbridge.security.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,43 +23,6 @@ public class AuthService{
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    public UserEntity signup(SignupRequestDTO req) {
-        // 아이디 중복 확인
-        if (userRepository.existsByUserId(req.getUserid())) {
-            throw new RuntimeException("이미 존재하는 아이디입니다.");
-        }
-
-        // 닉네임 중복 확인
-        if (userRepository.existsByNickname(req.getNickname())) {
-            throw new RuntimeException("이미 존재하는 닉네임입니다.");
-        }
-
-        // 전화번호 중복 확인
-        if (userRepository.existsByPhoneNumber(req.getPhoneNumber())) {
-            throw new RuntimeException("이미 등록된 전화번호입니다.");
-        }
-
-        // 비밀번호 확인
-        if (!req.getPassword().equals(req.getConfirmPassword())) {
-            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // 유저 저장
-        UserEntity user = UserEntity.builder()
-                .userid(req.getUserid())
-                .username(req.getUsername())
-                .nickname(req.getUsername())
-                .password(passwordEncoder.encode(req.getPassword()))
-                .phoneNumber(req.getPhoneNumber())
-                .gender(req.getGender())
-                .birthDate(req.getBirthDate())
-                .verified(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        return userRepository.save(user);
-    }
     public ApiResponseDTO<String> login(LoginRequestDTO req) {
         UserEntity user = userRepository.findByUserid(req.getUserid())
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 아이디입니다."));
