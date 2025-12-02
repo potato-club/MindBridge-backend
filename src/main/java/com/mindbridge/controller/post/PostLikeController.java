8b46ede76e94c585b1bde970845e744275ca44ec
budoli <als01540@gmail.com>
2025-12-03 00:42:46 +0900

    ref: path -> authentication

--- src/main/java/com/mindbridge/controller/post/PostLikeController.java
+++ src/main/java/com/mindbridge/controller/post/PostLikeController.java
@@ -30,9 +30,12 @@ public class PostLikeController {
     }
 
     @Operation(summary = "게시글 좋아요 조회 by 조민기")
-    @GetMapping("/liked/{userId}")
+    @GetMapping("/liked}")
     public ResponseEntity<List<PostResponseDto>> getLikedPosts(
-            @PathVariable Long userId) {
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        long userId = userDetails.getId();
+
         List<PostResponseDto> likedPosts = postLikeService.getLikedPosts(userId);
         return ResponseEntity.ok(likedPosts);
     }
